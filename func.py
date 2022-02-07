a6eab19cc9d8db9ef5d6f2971919c7e0ff1143cb
skruhi <98005870+skruhi@users.noreply.github.com>
2022-02-07 22:18:24 +0530

    Add files via upload

---
+++ func.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  7 22:14:50 2022
+
+@author: skruh
+"""
+
+def fun()
+fun()=most_frequent
+s = 'missisippi' 
+s_list=list(s) 
+print(s_list) 
+d_sort=sorted(s_list) 
+d_str=''.join(d_sort) 
+print(d_str) 
